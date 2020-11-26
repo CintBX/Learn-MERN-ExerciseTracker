38311b4d5f7160bbc3cbef34f314e6953bc22c73
Dustin Cintron <D.Cint88@gmail.com>
2020-11-25 21:28:45 -0500

    Useless comp agaain

--- src/components/UselessComp.js
+++ src/components/UselessComp.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class UselessComponent extends React.Component {
   render() {
     return (
-      <div>Wait! Don't be a negative nancy</div>
+      <div>Ok ok ok ok ok ok ok o okoko kok</div>
     );
   };
 };
